f24ad6539aa9fb36a146e02b46441a36f427e406
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-04-24 15:20:52 +0300

    react routers setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route path="/login">
+            {/* <Login /> */}
+            <h1>Login</h1>
+          </Route>
+          <Route path="/trivia">
+            <h1>Trivia</h1>
+          </Route>
+          <Route path="/scoreboard">
+            <h1>Scoreboard</h1>
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/login"/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
