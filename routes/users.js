ff604fd2e8a5d293ca90ccfcb1ca7356956a1942
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-05-02 18:45:04 +0300

    login

--- routes/users.js
+++ routes/users.js
@@ -7,21 +7,35 @@ const users = Router();
 const ACCESS_KEY = process.env.ACCESS_KEY;
 const REFRESH_KEY = process.env.REFRESH_KEY;
 
+const refreshTokens = [];
 
 users.post("/register", async (req, res, next) => {
     const {username, password} = req.body;
-    const isDetailsValid = typeof username === 'string' && typeof password === 'string' && username.length >= 3 && password.length >= 3; 
+    const isDetailsValid = typeof username === 'string' && typeof password === 'string' && username.length >= 3 && password.length >= 3;
     if (!isDetailsValid) return res.status(400).send('invalid username or password');
     const hashedPassword = bcrypt.hashSync(password, 10);
     try {
         const savedUserModel = await saveUser({username, password: hashedPassword});
-        const {name, score, createdAt} = savedUserModel.toJSON(); 
+        const {name, score, createdAt} = savedUserModel.toJSON();
         res.status(201).json({name, score, createdAt});
     } catch (e) {
         return res.status(409).send('username taken');
-    } 
+    }
 });
 
+users.post('/login', async (req, res, next) => {
+    const {username, password} = req.body;
+    const isDetailsValid = typeof username === 'string' && typeof password === 'string' && username.length >= 3 && password.length >= 3; 
+    if (!isDetailsValid) return res.status(403).send('invalid username or password');
+    const user = await getUser(username);
+    if (!user) return res.status(403).send('invalid username or password');
+    if( !bcrypt.compareSync(password, user.password) ) return res.status(403).send('invalid username or password');
+    const payload = {name: user.name, createdAt:user.createdAt};
+    const accessToken = jwt.sign( payload, ACCESS_KEY, {expiresIn: 10} ); 
+    const refreshToken = jwt.sign( payload, REFRESH_KEY);
+    refreshTokens.push(refreshToken);
+    res.status(200).json({accessToken, refreshToken});
+});
 
 function validator(req, res, next) {
     const accessToken = req.header('authorization');
