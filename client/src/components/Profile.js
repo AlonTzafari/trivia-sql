a22bcff1f728459af1926dff1df331df718a75de
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-05-17 20:18:39 +0300

    logout

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,9 +1,12 @@
 import {useContext, useEffect} from 'react'; 
 import {userContext} from '../globalContext';
 import axios from 'axios';
+import {useHistory} from 'react-router-dom';
+
 function Profile() {
     const {user, setUser} = useContext(userContext);
-     useEffect(() => {
+    const history = useHistory();
+    useEffect(() => {
          const config = {headers: {authorization: user.accessToken}};
          axios.get('/api/users/info', config)
          .then(res => {
@@ -12,10 +15,19 @@ function Profile() {
          })
      }, [])
 
+    const logoutHandler = () => {
+        const {refreshToken} = user;
+        axios.post('/api/users/logout', {refreshToken})
+        .then(() => {
+            history.push('/login');
+            setUser(null);
+        })
+    }
     return (
         <div>
             <h2>{user.name}</h2>
             <h2>{`SCORE: ${user.score}`}</h2>
+            <button onClick={logoutHandler}>log out</button>
         </div>
     )
 }
